95b5dd231b281dd93328618ccd0ca546bd277c6a
yoonhoShin <amuse1991@gmail.com>
2019-03-13 23:13:38 +0900

    login screen 작성
    NativeBase를 테스트하기 위해 간단한 login screen을 제작했습니다.

--- client/src/screens/login/Login.js
+++ client/src/screens/login/Login.js
@@ -1,33 +1,75 @@
 
 import React, {Component} from 'React';
-import { View, Text } from 'react-native';
-import {
-    Container,
-    Header,
-    Title,
-    Content,
-    Button,
-    Icon,
-    Body,
-    Left,
-    Right,
-    Input,
-    Item,
-    Form
-  } from "native-base";
+import {StyleSheet, View, TextInput, Dimensions, Text} from "react-native";
+import {Content, Form, Item, Input,Button} from 'native-base';
+import buttonStyle from '../../style/button.style';
+
+const {height, width} = Dimensions.get("window"); //전체 스크린의 width, height를 가져온다.
 
 export default class Login extends Component{
-    render(){
-        return(
-            <Container>
-                <Content>
-                    <Form>
-                        <Item>
-                            <Input placeholder={"Regular Textbox"}></Input>
-                        </Item>
-                    </Form>
-                </Content>
-            </Container>
-        )
-    }
+    // render(){
+    //     return(
+    //         <View style={loginScreenStyles.container}>
+    //             <Text style={loginScreenStyles.title}>스터디 어플리케이션</Text>
+    //             {/* <View style={loginScreenStyles.loginForm}>
+    //                 <TextInput></TextInput>
+    //             </View> */}
+    //             <Form>
+    //                 <Item>
+    //                     <Input placeholder="email"></Input>
+    //                 </Item>
+                    
+    //             </Form>
+    //         </View>
+            
+    //     )
+    // }
+    render() {
+        return (
+            <Content style={{backgroundColor:"black"}}>
+                <View style={loginScreenStyles.container}>
+                    <Text style={loginScreenStyles.title}>tttt</Text>
+                    
+                </View>
+                <Form>
+                    <Item>
+                        <Input placeholder="Username" />
+                    </Item>
+                    <Item last>
+                        <Input placeholder="Password" />
+                    </Item>
+                    <Button block dark>
+                        <Text style={buttonStyle.text_white}>로그인</Text>
+                    </Button>
+                    <Button block dark>
+                        <Text style={buttonStyle.text_white}>회원가입</Text>
+                    </Button>
+              </Form>
+              
+            </Content>
+        );
+      }
 }
+
+const loginScreenStyles = StyleSheet.create({
+    container:{
+        flex : 1,
+        backgroundColor : "black",
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    title:{
+        color:"white",
+        fontSize:30,
+        marginTop:40,
+        fontWeight:"200", //굵기
+        marginBottom: 30
+    },
+    loginForm:{
+        backgroundColor:"white",
+        flex:1,
+        width: width - 25, //전체 윈도우 width -25
+        borderRadius:10,
+        marginBottom:30
+    }
+})
