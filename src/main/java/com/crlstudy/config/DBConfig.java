130c315774f6871f7a926584fea1483be1c721af
yoonhoShin <amuse1991@gmail.com>
2019-03-15 20:49:31 +0900

    myBatis 추가
    프로젝트에 myBatis 추가

--- src/main/java/com/crlstudy/config/DBConfig.java
+++ src/main/java/com/crlstudy/config/DBConfig.java
@@ -2,10 +2,14 @@ package com.crlstudy.config;
 
 // spring jdbc 설정파일
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -20,6 +24,7 @@ public class DBConfig implements TransactionManagementConfigurer {
     private String url = "jdbc:mysql://localhost:3306/sampledb?useSSL=false";
     private String username = "jeffrey";
     private String password = "password";
+    private String mapperLocations = "classpath:META-INF/mybatis/mapper/**/*.xml";
 
     // spring jdbc 사용을 위한 dataSource 등록
     @Bean
@@ -32,6 +37,23 @@ public class DBConfig implements TransactionManagementConfigurer {
 
 
         return dataSource;
+    }
+
+    //mybatis 세션 등록
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        //sessionFactoryBean.setConfigLocation(new ClassPathResource(mybatisConfigLocation)); //http://june0313.github.io/2018/08/11/read-resource-file-on-spring/
+        sessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
+
+        return sessionFactoryBean.getObject();
+    }
+
+    // mybatis spring 연동모듈의 핵심. 코드에서 sqlSession을 대신한다.
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory){
+            return new SqlSessionTemplate(sqlSessionFactory);
     }
 
     @Override
