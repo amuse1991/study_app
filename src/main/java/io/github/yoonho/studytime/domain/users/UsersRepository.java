a90cc5882ff267343ca94f9a1d6da049c5f3b56b
yoonhoShin <amuse1991@gmail.com>
2019-03-20 11:02:14 +0900

    id 중복체크 코드 추가
    회원가입 API에 id 중복체크 코드 추가

--- src/main/java/io/github/yoonho/studytime/domain/users/UsersRepository.java
+++ src/main/java/io/github/yoonho/studytime/domain/users/UsersRepository.java
@@ -1,8 +1,14 @@
 package io.github.yoonho.studytime.domain.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+// 인터페이스를 정의하면 실행 시점에 spring data jpa가 구현 객체를 동적으로 생성해 주입
 public interface UsersRepository extends JpaRepository<Users,Long> {
     //JpaRepository<Entity클래스,PK타입>
     //기본적인 CRUD 메소드 자동 생성
+    @Query("select case when count(user_id)>0 then true else false end " +
+            "from Users u where user_id = :userId")
+    boolean existsByUserId(@Param("userId") String userId);
 }
--- src/main/java/io/github/yoonho/studytime/dto/users/UserInfoResponseDto.java
+++ src/main/java/io/github/yoonho/studytime/dto/users/UserInfoResponseDto.java
@@ -1,4 +1,4 @@
-package io.github.yoonho.studytime.dto;
+package io.github.yoonho.studytime.dto.users;
 
 import io.github.yoonho.studytime.domain.users.Users;
 import lombok.Getter;
@@ -8,9 +8,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class SignUpRequestDto {
+public class UserInfoResponseDto {
     private String user_id;
-    private String password;
     private String nickname;
     private String phone;
     private Integer point;
@@ -19,7 +18,6 @@ public class SignUpRequestDto {
     public Users toEntity(){
         return Users.builder()
                 .user_id(user_id)
-                .password(password)
                 .nickname(nickname)
                 .phone(phone)
                 .point(point)
